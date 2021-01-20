8867a536a6beec4815197e56b1e48827e49adf56
Mitchell Moore <mmoorejr24@gmail.com>
2021-01-20 14:10:03 -0800

    add a get route to server/index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Meower! 😀',
+  });
+});
+
 app.listen(5000, () => {
   console.log('Listening on port 5000');
 });
