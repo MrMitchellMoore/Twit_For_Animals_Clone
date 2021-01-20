9bfa23a30df472a035d9dc8d8b125a18a8c2c577
Mitchell Moore <mmoorejr24@gmail.com>
2021-01-20 14:36:40 -0800

    install cors and added to index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// routes
 app.get('/', (req, res) => {
   res.json({
     message: 'Meower! 😀',
@@ -12,6 +18,8 @@ app.post('/mews', (req, res) => {
   console.log(req.body);
 });
 
-app.listen(5000, () => {
+// fire up server
+app.listen(5000, (err) => {
+  if (err) console.log(err);
   console.log('Listening on port 5000');
 });
